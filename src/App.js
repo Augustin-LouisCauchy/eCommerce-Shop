5b2f494d1cbaeb103db2c6d5c05ada07e9377fd5
Mihail Nechaev <sadovni4ek@gmail.com>
2024-04-08 22:00:14 +0300

    images added && SUBstyles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,65 @@
 import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Items from "./components/Items";
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: [
+        {
+          id: 1,
+          title: "сhair gray",
+          img: "сhair-gray.jpeg",
+          desc: "Lorem ipsum dolor sit amet, consectetur adipisicing",
+          category: "chairs",
+          price: "49.99",
+        },
+        {
+          id: 2,
+          title: "table",
+          img: "table.jpg",
+          desc: "Lorem ipsum dolor sit amet, consectetur adipisicing",
+          category: "tables",
+          price: "149",
+        },
+        {
+          id: 3,
+          title: "sofa",
+          img: "sofa.jpg",
+          desc: "Lorem ipsum dolor sit amet, consectetur adipisicing",
+          category: "sofas",
+          price: "299.99",
+        },
+        {
+          id: 4,
+          title: "lamp",
+          img: "lamp.jpg",
+          desc: "Lorem ipsum dolor sit amet, consectetur adipisicing",
+          category: "lamps",
+          price: "15",
+        },
+        {
+          id: 5,
+          title: "сhair white",
+          img: "chair-white.jpg",
+          desc: "Lorem ipsum dolor sit amet, consectetur adipisicing",
+          category: "chairs",
+          price: "51.99",
+        },
+      ]
+    }
+  }
+  render() {
   return (
     <div className="wrapper">
        <Header/>
+       <Items items={this.state.items}/>
        <Footer/>
     </div>
   )
+  }
 }
 
 export default App;
