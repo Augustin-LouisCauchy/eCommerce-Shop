a6f0c6987c510840d4abf97c1b71624a55aa10cb
Mihail Nechaev <sadovni4ek@gmail.com>
2024-04-08 19:54:33 +0300

    styles for header added

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+        <div>
+            <span className='logo'>House Staff</span>
+            <ul className="nav">
+              <li className='bott'>About Us</li>
+              <li className='bott'>Contacts</li>
+              <li className='bott'>Account</li>
+            </ul>
+        </div>
+        <div className="presentation">
+
+        </div>
+    </header>
+  )
+}
