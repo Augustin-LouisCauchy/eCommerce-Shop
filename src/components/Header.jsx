fdd17b6d7f051f8210b7b5c1ba1f9bae8553e38e
Mihail Nechaev <sadovni4ek@gmail.com>
2024-04-09 20:33:09 +0300

    logic for cart added && some style for same

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaShoppingBag } from "react-icons/fa";
 
 export default function Header() {
+  let [cartOpen, setCartOpen] = useState(false)
+  
   return (
     <header>
         <div>
@@ -10,7 +13,14 @@ export default function Header() {
               <li className='bott'>Contacts</li>
               <li className='bott'>Account</li>
             </ul>
+            <FaShoppingBag onClick={() => setCartOpen(cartOpen = !cartOpen)} className={`shop-cart-button ${cartOpen && 'active'}`}/>
         </div>
+
+        {cartOpen && (
+          <div className="shop-cart">
+            
+          </div>
+        )}
         <div className="presentation">
 
         </div>
