f716d47ae7639e66cbf00bb68855a10952756c13
Mihail Nechaev <sadovni4ek@gmail.com>
2024-04-09 21:27:07 +0300

    logic for cart added ---> styles is next step

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,49 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { FaShoppingBag } from "react-icons/fa";
+import Order from "./Order";
 
-export default function Header() {
-  let [cartOpen, setCartOpen] = useState(false)
-  
+const showOrders = (props) => {                         /*{<-------------------Перебор всех товаров}*/
+  return (
+    <div>
+      {props.orders.map((el) => (
+        <Order key={el.id} item={el} />
+      ))}
+    </div>
+  );
+};
+
+const showNothing = () => {
+  return (
+    <div className="empty">
+      <h2>There are no goods</h2>
+    </div>
+  )
+}
+
+export default function Header(props) {
+  let [cartOpen, setCartOpen] = useState(false);
+
   return (
     <header>
-        <div>
-            <span className='logo'>House Staff</span>
-            <ul className="nav">
-              <li className='bott'>About Us</li>
-              <li className='bott'>Contacts</li>
-              <li className='bott'>Account</li>
-            </ul>
-            <FaShoppingBag onClick={() => setCartOpen(cartOpen = !cartOpen)} className={`shop-cart-button ${cartOpen && 'active'}`}/>
-        </div>
+      <div>
+        <span className="logo">House Staff</span>
+        <ul className="nav">
+          <li className="bott">About Us</li>
+          <li className="bott">Contacts</li>
+          <li className="bott">Account</li>
+        </ul>
+        <FaShoppingBag
+          onClick={() => setCartOpen((cartOpen = !cartOpen))}
+          className={`shop-cart-button ${cartOpen && "active"}`}
+        />
+      </div>
 
-        {cartOpen && (
-          <div className="shop-cart">
-            
-          </div>
-        )}
-        <div className="presentation">
-
+      {cartOpen && (
+        <div className="shop-cart">
+          {props.orders.length > 0 ? showOrders(props) : showNothing()}             {/*<----------Если есть элементы в корзине showOrders отрабатывает*/}
         </div>
+      )}
+      <div className="presentation"></div>
     </header>
-  )
+  );
 }
